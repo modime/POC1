397ec52836e0cd8fa97086beea3237a3b6402070
Marcus Olsson <marcus@modime.com>
2020-09-25 11:05:47 +0200

    Initial commit

---
+++ HelloWorldHelper/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace POC1.HelloWorldHelper
+{
+    public class HelloWorld
+    {
+        public static string GetGreeting()
+        {
+            return "Hello world! (4.7.1)";
+        }
+    }
+}
---
+++ POC1/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POC1.HelloWorldHelper;
+
+namespace POC1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            //return "Hello!";
+
+            return HelloWorld.GetGreeting();
+        }
+    }
+}
